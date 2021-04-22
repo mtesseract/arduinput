44ccaf0af60376399db852b088eb9d55c712b486
Moritz Clasmeier <mtesseract@silverratio.net>
2021-04-22 23:31:38 +0200

    Test CI

--- src/main.cpp
+++ src/main.cpp
@@ -21,11 +21,11 @@ void loop()
   digitalWrite(LED_BUILTIN, HIGH);
 
   // wait for a second
-  delay(500);
+  delay(100);
 
   // turn the LED off by making the voltage LOW
   digitalWrite(LED_BUILTIN, LOW);
 
    // wait for a second
-  delay(500);
+  delay(100);
 }
